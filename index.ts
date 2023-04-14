28fe6e59e02f075c2a65f5ea6607bd748a30547b
rustxjvmxgo <rustxjvmxgo@gmail.com>
2023-04-15 04:59:41 +0800

    Initial commit

---
+++ index.ts
@@ -0,0 +1,12 @@
+/// <reference types="@vertx/core" />
+// @ts-check
+
+vertx
+  .createHttpServer()
+  .requestHandler(function (req: any) {
+    req.response()
+      .putHeader("content-type", "text/plain")
+      .end("Hello!");
+  }).listen(3000);
+
+console.log('Server started on port 3000');
